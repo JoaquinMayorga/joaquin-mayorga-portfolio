a9d9ee36d943f03facce2a24d4d8f4553bf8f9b1
Joaquin Mayorga <joa.mayorga25@gmail.com>
2025-06-05 18:01:04 -0700

    experience done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ProfileComponent },
+      { path: 'experience', component: ExperienceComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  // This property will control whether the menu and icon have the 'open' class
+  isMenuOpen: boolean = false;
+
+  constructor() { }
+
+  /**
+   * Toggles the state of the hamburger menu.
+   * This method will be called when the hamburger icon or a menu link is clicked.
+   */
+  toggleMenu(): void {
+    // This single line does the equivalent of toggling 'open' on both
+    // the menu and icon, because they will both bind to this property.
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Menu state toggled. Menu is now:', this.isMenuOpen ? 'Open' : 'Closed');
+  }
+}
